500dc30470ae263dae55b3d89bf4dca5c2fd7883
Doriann <Doriann@DESKTOP-G5TR03M>
2025-06-30 16:55:31 -0500

    subiendo operaciones

--- src/cruds/Carros.java
+++ src/cruds/Carros.java
@@ -1,5 +1,25 @@
 package cruds;
 
 public class Carros {
-    
+   String Modelo,Capacidad; 
+
+    public Carros(String Modelo, String Capacidad) {
+        this.Modelo = Modelo;
+        this.Capacidad = Capacidad;
+        
+    }
+
+    public String getModelo() {
+        return Modelo;
+    }
+
+    public String getCapacidad() {
+        return Capacidad;
+    }
+
+    @Override
+    public String toString() {
+        return "Carros{" + "Modelo =" + Modelo + ", Capacidad=" + Capacidad + '}';
+        
+    }
 }
